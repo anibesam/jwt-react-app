fff4efcef889c12d755f01cbf76963268c76fc97
Anibe <samanibe@yahoo.com>
2023-07-19 14:00:25 +0100

    fix

--- src/Success.js
+++ src/Success.js
@@ -6,7 +6,10 @@ const Success = () => {
   return (
     <div className="success">
       <h2>Registration successful!</h2>
-      <p>You can now log in with your registered email and password.</p>
+      <p>You can now log in with your registered email and password. 
+
+        <a href="/">Login</a>
+      </p>
     </div>
   );
 };
